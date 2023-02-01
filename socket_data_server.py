631e56dd7dd435f217ba749bf9f59e8f6ec154d4
wonwoo KIM <54891281+dim-dol@users.noreply.github.com>
2023-02-01 17:17:02 +0900

    0201_v0.3
    Requirements Clear

--- socket_data_server.py
+++ socket_data_server.py
@@ -1,73 +1,87 @@
-import socket  # 소켓 프로그래밍에 필요한 API를 제공하는 모듈
-import struct  # 바이트(bytes) 형식의 데이터 처리 모듈
-import pickle  # ﻿객체의 직렬화 및 역직렬화 지원 모듈﻿
-import cv2  # OpenCV(실시간 이미지 프로세싱) 모듈
+import socket
+import struct
+import pickle
+import threading
+
+import cv2
+
+
+mouse_pressed = False
+s_x = s_y = e_x = e_y = p_x = p_y = -1
+drag_status = -1
+
+def mouse_click(event,x,y,flags,param):
+    global s_x,s_y,e_x,e_y,p_x,p_y,mouse_pressed, drag_status
+
+    if event==cv2.EVENT_LBUTTONDOWN:
+        mouse_pressed = True
+        s_x, s_y = x,y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if mouse_pressed: #drag
+            drag_status = 1
+        else:
+            p_x, p_y = x,y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        mouse_pressed = False
+        e_x,e_y = x,y
+
+        pos = str(s_x) + 'a' + str(s_y)
+
+        if drag_status == 1:
+            print("드래그 x: ", s_x, " ~ ", e_x, " y: ", s_y, " ~ ", e_y)
+            drag_status = 0
+            pos = pos + 'a' + str(e_x) + 'a' + str(e_y)
+
+        client_socket.send(pos.encode('utf-8'))
+
 
 # 서버 ip 주소 및 port 번호
-ip = '192.168.0.10'
+ip = '192.168.211.61'
 port = 50001
 
-# 소켓 객체 생성
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# 소켓 주소 정보 할당
 server_socket.bind((ip, port))
-
-# 연결 리스닝(동시 접속) 수 설정
 server_socket.listen(10)
 
-print('클라이언트 연결 대기')
+print('Client 연결 대기')
 
-# 연결 수락(클라이언트 (소켓, 주소 정보) 반환)
 client_socket, address = server_socket.accept()
-print('클라이언트 ip 주소 :', address[0])
+print('Client ip address :', address[0])
 
-# 수신한 데이터를 넣을 버퍼(바이트 객체)
 data_buffer = b""
 
-# calcsize : 데이터의 크기(byte)
-# - L : 부호없는 긴 정수(unsigned long) 4 bytes
 data_size = struct.calcsize("L")
 
 while True:
-    # 설정한 데이터의 크기보다 버퍼에 저장된 데이터의 크기가 작은 경우
+
     while len(data_buffer) < data_size:
         # 데이터 수신
         data_buffer += client_socket.recv(4096)
 
-    # 버퍼의 저장된 데이터 분할
     packed_data_size = data_buffer[:data_size]
     data_buffer = data_buffer[data_size:]
 
-    # struct.unpack : 변환된 바이트 객체를 원래의 데이터로 반환
-    # - > : 빅 엔디안(big endian)
-    #   - 엔디안(endian) : 컴퓨터의 메모리와 같은 1차원의 공간에 여러 개의 연속된 대상을 배열하는 방법
-    #   - 빅 엔디안(big endian) : 최상위 바이트부터 차례대로 저장
-    # - L : 부호없는 긴 정수(unsigned long) 4 bytes
     frame_size = struct.unpack(">L", packed_data_size)[0]
 
-    # 프레임 데이터의 크기보다 버퍼에 저장된 데이터의 크기가 작은 경우
     while len(data_buffer) < frame_size:
-        # 데이터 수신
         data_buffer += client_socket.recv(4096)
 
-    # 프레임 데이터 분할
+
     frame_data = data_buffer[:frame_size]
     data_buffer = data_buffer[frame_size:]
 
-    print("수신 프레임 크기 : {} bytes".format(frame_size))
-
-    # loads : 직렬화된 데이터를 역직렬화
-    # - 역직렬화(de-serialization) : 직렬화된 파일이나 바이트 객체를 원래의 데이터로 복원하는 것
     frame = pickle.loads(frame_data)
 
-    # imdecode : 이미지(프레임) 디코딩
-    # 1) 인코딩된 이미지 배열
-    # 2) 이미지 파일을 읽을 때의 옵션
-    #    - IMREAD_COLOR : 이미지를 COLOR로 읽음
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    frame = cv2.resize(frame,(1920,1080), interpolation=cv2.INTER_NEAREST)
 
     # 프레임 출력
+    cv2.namedWindow('Frame', cv2.WND_PROP_FULLSCREEN)
+    cv2.setMouseCallback('Frame', mouse_click)
+    cv2.setWindowProperty('Frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    key = cv2.waitKey(1)
     cv2.imshow('Frame', frame)
 
     # 'q' 키를 입력하면 종료
@@ -75,10 +89,8 @@ while True:
     if key == ord("q"):
         break
 
-# 소켓 닫기
 client_socket.close()
 server_socket.close()
 print('연결 종료')
 
-# 모든 창 닫기
 cv2.destroyAllWindows()
